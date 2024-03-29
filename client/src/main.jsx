cbb8d1bd4ed4af53b11a563d2969d3bc872b6444
Adrian Ortiz <147290741+Agerian@users.noreply.github.com>
2024-03-29 16:27:17 -0600

    Update package.json and add new components and pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+import Home from './pages/Home';
+import Menu from './pages/Menu';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Error from './pages/Error';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'menu', element: <Menu /> },
+      { path: 'about', element: <About /> },
+      { path: 'contact', element: <Contact /> },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,5 @@
+
+
 function Home() {
   return <div>Welcome to the Piedmont Home page BLAH!</div>;
 }
