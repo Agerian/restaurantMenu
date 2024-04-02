1556a08b78f86c08ac71741f3f5cc49d8815daca
SpatiallyAbsent <joshuapdow@gmail.com>
2024-04-02 17:32:42 -0600

    save incase of pull problems

---
+++ client/src/components/Signup.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+function Signup({ onSignup }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Call signup function passed from parent component
+    onSignup({ name, email, password });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      </label>
+      <label>
+        Email:
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </label>
+      <button type="submit">Signup</button>
+    </form>
+  );
+}
+
+export default Signup;
